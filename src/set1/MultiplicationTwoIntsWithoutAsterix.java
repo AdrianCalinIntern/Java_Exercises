2fe2185cc60b552f0684a28092b59b9960e531cd
Adrian Urate <adrian.calin991@gmail.com>
2021-03-13 12:28:24 +0200

    Updated the multiplicationCalculator method + Added integer verification in main method

--- src/set1/MultiplicationTwoIntsWithoutAsterix.java
+++ src/set1/MultiplicationTwoIntsWithoutAsterix.java
@@ -1,22 +1,30 @@
 package set1;
 import java.lang.Math;
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
+/*
+Write a Java program to multiply two given integers without using the multiply operator(*).
+ */
 
 public class MultiplicationTwoIntsWithoutAsterix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the the first number : \n");
-        int number1 = scanner.nextInt();
-
-        System.out.print("Enter the the second number : \n");
-        int number2 = scanner.nextInt();
-
-        int multiplicationOutput = multiplicationCalculator(number1,number2);
-        System.out.println("Result: " + multiplicationOutput);
-
+        try {
+            System.out.print("Enter the the first number : \n");
+            int number1 = scanner.nextInt();
+            System.out.print("Enter the the second number : \n");
+            int number2 = scanner.nextInt();
+            System.out.println(multiplicationCalculator(number1,number2));
+        }catch (Exception ex){
+            System.out.println("Value is not an int");
+        }
     }
-    public static int multiplicationCalculator(int number1, int number2){
-        return Math.multiplyExact(number1,number2);
+    public static int multiplicationCalculator(int number, int howManyTimes){
+        int result = 0;
+        for (int i = 1; i < howManyTimes; i++){
+            result = number = number + howManyTimes;
+        }
+        return result;
     }
 }
