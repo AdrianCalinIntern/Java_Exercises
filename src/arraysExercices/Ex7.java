61d2ff934194fa266821bc2fadda63e4a8a4dd97
Adrian Urate <adrian.calin991@gmail.com>
2021-04-07 22:50:52 +0300

    Created Ex7: Write a Java program to iterate through all elements in a array list and print them.

---
+++ src/arraysExercices/Ex7.java
@@ -0,0 +1,24 @@
+package arraysExercices;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.StreamSupport;
+
+public class Ex7 {
+    // Write a Java program to iterate through all elements in a array list and print them.
+    public static void main(String[] args) {
+        ArrayList<Integer> myArray = new ArrayList<>();
+        myArray.add(1);
+        myArray.add(2);
+        myArray.add(3);
+        myArray.add(4);
+        myArray.add(5);
+        myArray.add(6);
+        myArray.add(7);
+
+        for (int i = 0; i<myArray.size(); i++){
+            System.out.println(myArray.get(i));
+        }
+
+    }
+}
