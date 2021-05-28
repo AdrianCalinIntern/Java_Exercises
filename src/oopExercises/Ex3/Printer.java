c0bfa8563a3805000c58ac5b8b99d36c19c58c01
Adrian Urate <adrian.calin991@gmail.com>
2021-05-28 14:57:28 +0300

    Created OOP.Ex3 package and solved the 3nd problem

--- src/oopExercises/Ex3/Printer.java
+++ src/oopExercises/Ex3/Printer.java
@@ -1,4 +1,36 @@
 package oopExercises.Ex3;
 
 public class Printer {
+
+    private int TonerLevel = 20;
+    private int pagesPrinted = 90;
+    private boolean isDuplexPrinter;
+
+    public Printer(boolean printerType)
+    {
+        this.isDuplexPrinter = printerType;
+    }
+
+
+    public void fillThePrinter(int value){
+        if(TonerLevel + value > 100){
+            System.out.println("The printer is full");
+        }
+        else{
+            TonerLevel = TonerLevel + value;
+        }
+    }
+
+    public void pagesToPrint(int value){
+        if (value>0){
+            pagesPrinted = pagesPrinted + value;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Toner = " + TonerLevel + "\n" +
+                "Pages to print = " + pagesPrinted + "\n";
+    }
 }
